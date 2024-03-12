0c70365c8d59908f5f4c7946834abea3199d4be3
riteshpinnacle <riteshgupta@ssccglpinnacle.com>
2024-03-12 10:56:31 +0530

    This project is for test purpose...

---
+++ app/src/main/java/com/example/barcodescanner/BarcodeScan.kt
@@ -0,0 +1,87 @@
+package com.example.barcodescanner
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.SurfaceHolder
+import android.widget.Toast
+import com.example.barcodescanner.databinding.ActivityBarcodeScanBinding
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
+import java.io.IOException
+
+class BarcodeScan : AppCompatActivity() {
+    private lateinit var binding: ActivityBarcodeScanBinding
+    private lateinit var barcodeDetector: BarcodeDetector
+    private lateinit var cameraSource: CameraSource
+    var intentData = ""
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBarcodeScanBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun iniBc(){
+        barcodeDetector = BarcodeDetector.Builder(this)
+            .setBarcodeFormats(Barcode.ALL_FORMATS)
+            .build()
+        cameraSource = CameraSource.Builder(this,barcodeDetector)
+            .setRequestedPreviewSize(1920,1080)
+            .setAutoFocusEnabled(true)
+//            .setFacing(CameraSource.CAMERA_FACING_BACK)
+               .build()
+        binding.surfaceView!!.holder.addCallback(object: SurfaceHolder.Callback{
+            @SuppressLint("MissingPermission")
+            override fun surfaceCreated(holder: SurfaceHolder) {
+                try {
+                    cameraSource.start(binding.surfaceView!!.holder)
+                }catch (e:IOException){
+                    e.printStackTrace()
+                }
+            }
+
+            override fun surfaceChanged(
+                holder: SurfaceHolder,
+                format: Int,
+                width: Int,
+                height: Int
+            ) {
+
+            }
+
+            override fun surfaceDestroyed(holder: SurfaceHolder) {
+                cameraSource.stop()
+            }
+
+        })
+        barcodeDetector.setProcessor(object : Detector.Processor<Barcode>{
+            override fun release() {
+                Toast.makeText(applicationContext, "barcode scanner has been stopped",
+                    Toast.LENGTH_LONG).show()
+            }
+
+            override fun receiveDetections(p0: Detector.Detections<Barcode>) {
+                val barcodes = p0.detectedItems
+                if(barcodes.size()!=0){
+                    binding.txtBarcodeValue!!.post{
+                        binding.btnAction!!.text = "SEARCH ITEM"
+                        intentData = barcodes.valueAt(0).displayValue
+                        binding.txtBarcodeValue.setText(intentData)
+//                        finish()
+                    }
+                }
+            }
+
+        })
+    }
+    override fun onPause(){
+        super.onPause()
+        cameraSource!!.release()
+    }
+    override fun onResume(){
+        super.onResume()
+        iniBc()
+    }
+}
